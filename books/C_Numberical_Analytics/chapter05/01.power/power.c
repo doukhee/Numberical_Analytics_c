aa040d9ae32ffc1297d84b3832bac7b94d64db13
doukhee <fain9301@yahoo.com>
2021-04-06 21:49:25 +0900

    feat: chapter05 01.power source add

---
+++ books/C_Numberical_Analytics/chapter05/01.power/power.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+
+double A[50][50];
+double x[50];
+
+void Power(int matrixSize, int Max);
+
+int main(int argc, char **argv){
+    int Max, matrixSize;
+    printf("Max Repeat Number ? ");
+    scanf("%d", &Max);
+    printf("\r\nMatrix Size ? ");
+    scanf("%d", &matrixSize);
+    printf("\r\nMatrix Element ? \r\n");
+    for(int i = 0 ; i < matrixSize; i++){
+        printf("%d row : ", i + 1);
+        for(int j = 0; j < matrixSize; j++){
+            scanf("%lf", &A[i][j]);
+        }
+    }
+    printf("\r\nInit vector ? ");
+    for(int i = 0; i < matrixSize; i++){
+        scanf("%lf", &x[i]);
+    }
+    Power(matrixSize, Max);
+    printf("\r\n");
+    return 0;
+}
+
+void Power(int matrixSize, int Max){
+    double y[50];
+    double eigval, eta, sum;
+    for(int k = 1; k <= Max; k++){
+        printf("\r\nk = %d \r\n", k);
+        for(int i = 0; i < matrixSize; i++){
+            sum = 0.;
+            for(int j = 0; j < matrixSize; j++){
+                sum = sum + A[i][j] * x[j];
+            }
+            y[i] = sum;
+        }
+        sum = 0.;
+        for(int i = 0 ; i < matrixSize; i++){
+            sum = sum + x[i] * y[i];
+        }
+        printf("\t고유치는 : %lf \r\n", eigval = sum);
+        sum = 0.;
+        for(int i = 0; i < matrixSize; i++){
+            sum = sum + y[i] * y[i];
+        }
+        eta = pow(sum, 0.5);
+        printf("\t 고유 벡터는 : ");
+        for(int i = 0; i < matrixSize; i++){
+            printf("%lf \t", x[i] = y[i]/eta);
+        }
+    }
+}
