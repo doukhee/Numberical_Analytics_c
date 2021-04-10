8f74d8c841bcfe79709b595fbb0aa84e3833944e
doukhee <fain9301@yahoo.com>
2021-04-11 01:25:47 +0900

    feat: power solution explain add

--- books/C_Numberical_Analytics/chapter05/01.power/power.c
+++ books/C_Numberical_Analytics/chapter05/01.power/power.c
@@ -39,50 +39,52 @@ int main(int argc, char **argv){
     return 0;
 }
 
-/** */
+/** 거듭제곱 방법으로 방정식 푸는 함수 */
 void Power(int matrixSize, int Max){
-    /** */
+    /** 결과를 저장 하기 위한 변수 선언 */
     double y[50];
-    /** */
+    /** 고유치, 고유값, 행렬 계산의 값을 저장하기 위한 합 변수 선언 */
     double eigenValue, eta, sum;
-    /** */
+    /** y = A[(k-1)] 계산 */
+    /** 최대 횟수만큼 반복 하기 위한 반복문 */
     for(int k = 1; k <= Max; k++){
-        /** */
+        /** 몇번째 실행인지 알려주기 위한 출력 */
         printf("\r\nk = %d \r\n", k);
-        /** */
+        /** 행렬의 행만큼 반복 */
         for(int i = 0; i < matrixSize; i++){
-            /** */
+            /** 합변수를 0.으로 초기화 */
             sum = 0.;
-            /** */
+            /** 행렬의 열만큼 반복 */
             for(int j = 0; j < matrixSize; j++){
-                /** */
+                /** 행렬의 곱의 값을 저장 */
                 sum = sum + A[i][j] * x[j];
             }
-            /** */
+            /** 행렬의 곱 계산 값을 임시 저장 */
             y[i] = sum;
         }
-        /** */
+        /** 합변수를 0.으로 초기화 */
         sum = 0.;
-        /** */
-        for(int i = 0 ; i < matrixSize; i++){
-            /** */
+        /** 행렬읠 행만큼 반복 */
+        for(int i = 0; i < matrixSize; i++){
+            /** 초기 값과 행렬의 곱 계산의 내적 값 구히기 */
             sum = sum + x[i] * y[i];
         }
-        /** */
+        /** 고유치를 출력 하기 */
         printf("\t고유치는 : %lf \r\n", eigenValue = sum);
-        /** */
+        /** 행렬의 고유 벡터를 구하기 위한 합 변수 초기화 */
         sum = 0.;
-        /** */
+        /** 행렬의 크기만큼 반복 */
         for(int i = 0; i < matrixSize; i++){
-            /** */
+            /** 행렬의 곱의 제곱 노름값 구하기 */
             sum = sum + y[i] * y[i];
         }
-        /** */
+        /** 노름값의 루트 값을 저장 */
         eta = pow(sum, 0.5);
-        /** */
+        /** 고유벡터 출력을 알려주기 위한 출력 */
         printf("\t 고유 벡터는 : ");
-        /** */
+        /** 행렬의 행만큼 반복 */
         for(int i = 0; i < matrixSize; i++){
+            /** 행렬의 고유 벡터 출력 */
             printf("%lf \t", x[i] = y[i]/eta);
         }
     }
